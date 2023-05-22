9b61953ee2f87ac0be5c5db87f73a23f99413a25
nalthirumeni <40792399+nalthirumeni@users.noreply.github.com>
2023-05-22 18:21:25 +0530

    Update mtl_func.c

--- mtl_func.c
+++ mtl_func.c
@@ -35,7 +35,8 @@ both csv1.txt and mtl.txt to be kept in the same directory of the exe file.
 #include <string.h>
 
 #define BUFFER_SIZE 10000  //
-#define BUFFER_2_SIZE 20
+#define BUFFER_2_SIZE 200
+#define BUFFER_SIZE_U 1000 // for until
 
 char line[100];
 int num_formulas = 6; // change this to the number of formulas in your file
@@ -49,6 +50,16 @@ int buffer_f[BUFFER_SIZE] = {0};
 int front = 0, rear = 0;
 int front_f = 0, rear_f = 0;
 int fid;
+
+int buffer_u_p1[BUFFER_SIZE_U];
+int buffer_u_p2[BUFFER_SIZE_U];
+int buffer_u_timestamp[BUFFER_SIZE_U];
+int rear_u_p1 = -1;
+int rear_u_p2 = -1;
+int U_satisfied_flag =0;
+
+int q_full=0;
+int flag_q =0;
 
 
 //Function declarations here.
@@ -129,7 +140,7 @@ printf("\nAll MTL formuals read.\n");
 
     sscanf(line, "%d,%d,%d,%d,%d,%d", &time, &p1, &p2, &p3, &p4, &p5);
 
-
+/*
 
 //MTL_G
     int mtl_g_output = MTL_G(p2, send_interval);
@@ -196,8 +207,27 @@ printf("\nAll MTL formuals read.\n");
 
             }
 
+*/
+    //MTL_U
+
+    //int MTL_U(int p1, int p2, int start_time,int end_time)
+
+   // printf("\nbefore until func. calling ... rear_u_p1=%d,rear_u_p2=%d\n",rear_u_p1,rear_u_p2);
+
+    printf("\n");
+    printf("\nTimestamp: %d", time);
+    printf("\np: %d, q %d\n", p1,p2);
+    int mtl_u_output = MTL_U(p2,p3,3,7);
+    printf("\nMTL_Until Output %d",mtl_u_output);
+
+
+
+
+
 
     }  //while ends here
+
+
 
 
         fclose(fp);
@@ -212,13 +242,17 @@ printf("\nAll MTL formuals read.\n");
 // Function definitions here.
 
 
+
+// Globally function
+
+
 int MTL_G(int p, int interval) {
     int i;
 
     buffer_g[rear] = p;
     rear = (rear + 1) % BUFFER_SIZE;
 
-    if ((rear + BUFFER_SIZE - front) % BUFFER_SIZE > interval) {
+    if ( ((rear + BUFFER_SIZE - front) % BUFFER_SIZE) > interval) {
         front = (front + 1) % BUFFER_SIZE;
     }
 
@@ -236,6 +270,7 @@ int MTL_G(int p, int interval) {
 }
 
 
+// Finally funtion
 
 int MTL_F(int p, int interval) {
     int i;
@@ -265,4 +300,152 @@ int MTL_F(int p, int interval) {
         return -1;
     }
 }
+
+
+
+// Until function.
+//#define BUFFER_SIZE interval
+
+
+
+int MTL_U(int p1, int p2, int start_time,int end_time)
+
+
+{
+
+   int interval =(end_time-start_time)+1;
+   printf("\nInterval%d",interval);
+
+   rear_u_p1 = (rear_u_p1 + 1) % interval;
+   buffer_u_p1[rear_u_p1] = p1;
+
+   rear_u_p2 = (rear_u_p2 + 1) % interval;
+   buffer_u_p2[rear_u_p2] = p2;
+
+   buffer_u_timestamp[rear_u_p2] = time;
+
+
+    printf("\n U_satisfied_flag =%d, q_full=%d,",U_satisfied_flag,q_full);
+
+    printf("\nrear_u_p1=%d,rear_u_p2=%d, interval = %d",rear_u_p1,rear_u_p2,interval);
+
+    if ((rear_u_p1 == (interval-1)) && (rear_u_p2 == (interval-1)) && (flag_q == 0) )
+
+    {
+       printf("\n First time Until_Que_full");
+       q_full =1;
+       flag_q =1;
+    }
+    else if (q_full == 0)
+
+    {
+            printf ("\nWAIT to fill.....Until_Que,q_full =%d ",q_full);
+
+    }
+
+         if (q_full == 1)
+
+         {
+
+        // compare p1 p2 for until condition here for every time stamp once q filled..........
+
+        U_satisfied_flag=0;
+
+        printf("\n Before - U_satisfied_flag =%d, q_full=%d,",U_satisfied_flag,q_full);
+
+        printf("\n Until evaluation range , Row#1:Time stamp,Row#2: p1,Row#3: p2\n\n");
+         for (int m1=0; m1<=interval-1 ;m1++)
+
+         {
+         printf("%d,",buffer_u_timestamp[m1]);
+         }
+         printf("\n");
+
+          for (int m11=0; m11<=interval-1 ;m11++)
+
+         {
+         printf("%d,",buffer_u_p1[m11]);
+         }
+         printf("\n");
+
+          for (int m111=0; m111<=interval-1 ;m111++)
+
+         {
+         printf("%d,",buffer_u_p2[m111]);
+         }
+
+         printf("\n");
+
+
+        for (int m=0; m<=interval-1 ;m++)
+
+        {
+
+            printf ("\n Inside for..");
+
+            if ((buffer_u_p1[m] == 1) &&  (buffer_u_p2[m] == 1) && (U_satisfied_flag ==0))
+
+            {
+            printf ("\n Inside if p1 q1.... satisfied");
+            printf ("\nResult for timestamp = %d, p1=%d until p2 =%d", (time-end_time),p1,p2,((time-end_time)+start_time),((time-end_time)+end_time));
+            printf ("\np1=%d until p2 =%d SATISFIED at %d,",buffer_u_p1 [m],buffer_u_p2[m]),(buffer_u_timestamp[m]);
+            U_satisfied_flag =1; // just to print only once for U satisfaction.
+            return 1;
+            }
+
+             if ((buffer_u_p1[m] == 0) &&  (buffer_u_p2[m] == 1) && (U_satisfied_flag ==0))
+            {
+             printf ("\n Inside if p0 q1.... satisfied");
+             printf ("\nResult for timestamp = %d, p1=%d until p2 =%d", (time-end_time),p1,p2,((time-end_time)+start_time),((time-end_time)+end_time));
+            printf ("\np1=%d until p2 =%d SATISFIED at %d,",buffer_u_p1 [m],buffer_u_p2[m]),(buffer_u_timestamp[m]);
+            U_satisfied_flag =1; // just to print only once for U satisfaction.
+            return 1;
+            }
+
+            if ((buffer_u_p1[m] == 0) &&  (buffer_u_p2[m] == 0) && (U_satisfied_flag ==0))
+            {
+            printf ("\n Inside if p0 q0.... Failed");
+            printf ("\nResult for timestamp = %d, p1=%d until p2 =%d", (time-end_time),p1,p2,((time-end_time)+start_time),((time-end_time)+end_time));
+            printf ("\nUntil FAILED AT = %d.....",(buffer_u_timestamp[m]));
+            return 0;
+            }
+
+
+
+           if ((buffer_u_p1[m] == 1) &&  (buffer_u_p2[m] == 0) && (U_satisfied_flag ==0)) //next..wait .case...
+
+            {
+              printf ("\n Inside if p1 q0.... MAY BE...");
+          //  printf ("\nResult for timestamp = %d, p1=%d until p2 =%d", (time-end_time),p1,p2,((time-end_time)+start_time),((time-end_time)+end_time));
+           // printf ("\np1 until p2  next ..wait...");
+          //  return -1;
+            }
+
+            else if (U_satisfied_flag == 1) //already satisifed...
+
+            {
+                printf ("\nUntil  already satisfied......");
+            }
+
+            printf("\n after for loop U_satisfied_flag =%d, q_full=%d,",U_satisfied_flag,q_full);
+
+
+
+         }
+
+
+
+
+        }
+
+
+
+
+      //
+        return -1;
+
+            }
+
+
+
 
